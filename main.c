56a3fee6fbe911742172b016a2cf398cdf357ca0
hioikawa <101631407+uminomae@users.noreply.github.com>
2022-08-07 15:08:02 +0900

    new_type_tetriminoに引数変更

--- main.c
+++ main.c
@@ -6,24 +6,27 @@ suseconds_t timer = 400000;
 int decrease = 1000;
 t_tetrimino current;
 
-void write_array(t_tetrimino *figure, char **copy_type_tetrimino_figure)
+void write_array(t_tetrimino *new_type_tetrimino, char **copy_type_tetrimino_figure)
 {
-	const int n = figure->width_and_height;
+	const int n = new_type_tetrimino->width_and_height;
 	//int x, y;
 	for(int x = 0; x < n; x++){
 	//for(x = 0; x < figure->width_and_height; x++){
-		figure->figure[x] = (char*)malloc(sizeof(char) * n);
+		new_type_tetrimino->figure[x] = (char*)malloc(sizeof(char) * n);
 		//figure->figure[x] = (char*)malloc(figure->width_and_height*sizeof(char));
 		for(int y = 0; y < n; y++) {
 		//for(y = 0; y < figure->width_and_height; y++) {
-			figure->figure[x][y] = copy_type_tetrimino_figure[x][y];
+			new_type_tetrimino->figure[x][y] = copy_type_tetrimino_figure[x][y];
 		}
 	}
 }
 
 t_tetrimino create_shape(t_tetrimino type_tetrimino){
+	const int n = type_tetrimino.width_and_height;
 	t_tetrimino new_type_tetrimino = type_tetrimino;
-	new_type_tetrimino.figure = (char**)malloc(new_type_tetrimino.width_and_height*sizeof(char*));
+
+	new_type_tetrimino.figure = (char**)malloc(sizeof(char *) * n);
+	//new_type_tetrimino.figure = (char**)malloc(new_type_tetrimino.width_and_height*sizeof(char*));
 	write_array(&new_type_tetrimino, type_tetrimino.figure);
     return (new_type_tetrimino);
 }
