5cb1e42c739bd2221fce4c54b1160588022bb2a7
hioikawa <101631407+uminomae@users.noreply.github.com>
2022-08-09 00:36:50 +0900

    erase.c

--- main.c
+++ main.c
@@ -70,42 +70,42 @@ void fix_tetrimino_on_the_field(t_tetris *tetris){
 	}
 }
 
-int count_blocks_of_line(t_tetris *tetris, int y){
-	int blocks = 0;
+//int count_blocks_of_line(t_tetris *tetris, int y){
+//	int blocks = 0;
 
-	for(int x = 0; x < FIELD_COL; x++) {
-		blocks += tetris->playing_field[y][x];
-	}
-	return (blocks);
-}
+//	for(int x = 0; x < FIELD_COL; x++) {
+//		blocks += tetris->playing_field[y][x];
+//	}
+//	return (blocks);
+//}
 
-void lower_the_upper_block(t_tetris *tetris, int y){
-	for( ; y >= 1; y--)
-		for(int x = 0; x < FIELD_COL; x++)
-			tetris->playing_field[y][x] = tetris->playing_field[y-1][x];
-}
+//void lower_the_upper_block(t_tetris *tetris, int y){
+//	for( ; y >= 1; y--)
+//		for(int x = 0; x < FIELD_COL; x++)
+//			tetris->playing_field[y][x] = tetris->playing_field[y-1][x];
+//}
 
-void clear_line(t_tetris *tetris, int y){
-	for(int x = 0; x < FIELD_COL; x++)
-		tetris->playing_field[y][x] = 0;
-}
+//void clear_line(t_tetris *tetris, int y){
+//	for(int x = 0; x < FIELD_COL; x++)
+//		tetris->playing_field[y][x] = 0;
+//}
 
-void drop_placed_block_one_rank(t_tetris *tetris, int y){
-	lower_the_upper_block(tetris, y);
-	clear_line(tetris, TOP_ROW);
-}
+//void drop_placed_block_one_rank(t_tetris *tetris, int y){
+//	lower_the_upper_block(tetris, y);
+//	clear_line(tetris, TOP_ROW);
+//}
 
-int count_completed_lines_and_erase(t_tetris *tetris){
-	int number_of_completed_lines = 0;
-	for(int y = 0; y < FIELD_ROW; y++){
-		if(count_blocks_of_line(tetris, y) == FIELD_COL){
-			drop_placed_block_one_rank(tetris, y);
-			tetris->time_to_update -= tetris->decrease--;
-			number_of_completed_lines++;
-		}
-	}
-	return (number_of_completed_lines);
-}
+//int count_completed_lines_and_erase(t_tetris *tetris){
+//	int number_of_completed_lines = 0;
+//	for(int y = 0; y < FIELD_ROW; y++){
+//		if(count_blocks_of_line(tetris, y) == FIELD_COL){
+//			drop_placed_block_one_rank(tetris, y);
+//			tetris->time_to_update -= tetris->decrease--;
+//			number_of_completed_lines++;
+//		}
+//	}
+//	return (number_of_completed_lines);
+//}
 
 void make_next_tetrimino(t_tetris *tetris, t_tetrimino *tetlimino){
 //void make_next_tetrimino(t_tetris *tetris){
