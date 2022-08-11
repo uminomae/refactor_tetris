72ff887f28c702bfaac16b628ff5a8b4eeae4515
hioikawa <101631407+uminomae@users.noreply.github.com>
2022-08-11 16:25:36 +0900

    put_screen作業開始

--- tetris.c
+++ tetris.c
@@ -6,7 +6,6 @@
 #define R 20
 #define C 15
 
-//int can_move_tetrimino(t_tetris *tetris, t_tetrimino shape);
 void rotate_clodkwise(t_tetrimino shape);
 void put_screen(t_tetris *tetris, t_tetrimino *current);
 int hasToUpdate();
@@ -26,7 +25,6 @@ void begin_game(t_tetris *tetris, t_tetrimino *current, t_tetrimino *type){
 	(void)tetris;
 	*current = create_new_tetrimino(type);
 	judge_the_end_of_game(tetris, *current);
-	//judge_the_end_of_game(tetris);
     //refresh_game_screen(tetris, tetris->tetrimino);
 }
 
@@ -45,10 +43,6 @@ int main() {
 
 
     int c;
-	//judge_the_end_of_game(&tetris, current)
-	//if(!can_move_tetrimino(&tetris, current)){
-	//	tetris.game_status = GAME_OVER;
-	//}
     put_screen(&tetris, &current);
 	while(tetris.game_status == IN_GAME){
 		if ((c = getch()) != ERR) {
@@ -190,6 +184,70 @@ int main() {
     
 	return 0;
 }
+
+
+
+static void print_string_to_window(char *str,...){
+	printw(str);
+}
+
+//--------------------------------------------------------
+// refresh_game_screen
+//--------------------------------------------------------
+static void get_current_position(t_tetris *tetris, \
+							char next_playing_field[FIELD_ROW][FIELD_COL]){
+	const int n = tetris->tetrimino->width_and_height;
+
+	for(int i = 0; i < n ;i++){
+		for(int j = 0; j < n ; j++){
+			if(tetris->tetrimino->figure[i][j])
+				next_playing_field[tetris->tetrimino->row+i][tetris->tetrimino->col+j] = tetris->tetrimino->figure[i][j];
+		}
+	}
+}
+
+static void print_header(){
+	for(int i = 0; i < FIELD_COL - 9; i++)
+		print_string_to_window(" ");
+	print_string_to_window("42 Tetris\n");
+}
+
+static void print_game_field(t_tetris *tetris, \
+						char next_playing_field[FIELD_ROW][FIELD_COL]){
+	for(int i = 0; i < FIELD_ROW ;i++){
+		for(int j = 0; j < FIELD_COL ; j++){
+			if (tetris->playing_field[i][j] + next_playing_field[i][j])
+				print_string_to_window("%c ", '#');
+			else
+				print_string_to_window("%c ", '.');
+		}
+		print_string_to_window("\n");
+	}
+}
+
+static void print_footer(t_tetris *tetris){
+	print_string_to_window("\nScore: %d\n", tetris->score);
+}
+
+static void print_game_screen(t_tetris *tetris, \
+						char next_playing_field[FIELD_ROW][FIELD_COL]){
+	print_header();
+	print_game_field(tetris, next_playing_field);
+	print_footer(tetris);
+}
+
+void put_screen(t_tetris *tetris, t_tetrimino *current){
+//void refresh_game_screen(t_tetris *tetris, t_tetrimino *current){
+	char next_playing_field[FIELD_ROW][FIELD_COL] = {0};
+	
+	tetris->tetrimino = current;
+	get_current_position(tetris, next_playing_field);
+	clear();
+	print_game_screen(tetris, next_playing_field);
+}
+//--------------------------------------------------------
+// refresh_game_screen
+//--------------------------------------------------------
 
 
 void put_screen(t_tetris *tetris, t_tetrimino *current){
